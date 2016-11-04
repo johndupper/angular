d4909c8a9cae2383c8d435477cf0c6f8061f4dd9
John Dupper <john.dupper@me.com>
2016-11-04 18:47:37 -0400

    created controller, assigned it to body element, assigned element to callback function

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,9 @@
 // create new angular module, pass an empty array as the argument
 // without the [], angular thinks we are trying to call an existing
 // module instead of creating one:
-angular.module('todoListApp', []);
+angular.module('todoListApp', [])
+.controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+        console.log("Hello from the mainCtrl!");
+    };
+});
