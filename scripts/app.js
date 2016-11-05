1aecf15f13ff32b153ebe67f6ce65689ba989313
John Dupper <john.dupper@me.com>
2016-11-05 14:19:31 -0400

    added a controller to our module, attached to to a body element

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,13 @@
-// create new angular module, pass an empty array as the argument
-// without the [], angular thinks we are trying to call an existing
-// module instead of creating one:
-angular.module('todoListApp', [])
-.controller('mainCtrl', function($scope) {
+
+angular.module('todoListApp', []).controller('mainCtrl', function($scope) {
     $scope.helloWorld = function() {
         console.log("Hello from the mainCtrl!");
     };
 });
+
+``
+// to create an angular module, pass an empty array as the argument
+// without the [], angular thinks we are trying to call an existing module instead of creating one
+
+// .controller() is how we attach logic to our module
+// a controller function is essentially a property of our module
